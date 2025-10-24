d031c8920d26a4f0a9d7e4712b86589d35ebaf83
cketiel <cketielbr@gmail.com>
2025-10-23 20:52:04 -0600

    add ncra11 example

---
+++ ncra11_to_json.py
@@ -0,0 +1,15 @@
+import requests
+from api_key import key
+import json
+
+FII = "NCRA11"
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={FII}.SAO&apikey={key}'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+FILE_NAME = "NCRA11.json"
+FILE_PATH = os.path.join("json", FILE_NAME)
+
+with open(FILE_PATH, 'w') as file:
+    json.dump(data, file, indent=2)
