bc1f089225b630a43b7f06184a10419a28a6efc3
cketiel <cketielbr@gmail.com>
2025-10-23 20:49:16 -0600

    add list_2_4

---
+++ list_2_4.py
@@ -0,0 +1,30 @@
+import requests
+from api_key import key
+import pandas as pd
+from io import StringIO
+import os
+
+# List of stocks
+stoks = ["AGRX11", "RZAG11", "XPCA11", "NCRA11"]
+
+# Create DataFrame
+table = pd.DataFrame()
+
+# Ensure the "csv" folder exists
+output_folder = "csv"
+os.makedirs(output_folder, exist_ok=True)
+
+# Fetch data and concatenate
+for stok in stoks:
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stok}.SAO&apikey={key}&datatype=csv'
+    r = requests.get(url)
+    row = pd.read_csv(StringIO(r.text))
+    table = pd.concat([table, row], ignore_index=True)
+
+# Path to save CSV
+csv_file_path = os.path.join(output_folder, "list_2_4.csv")
+
+# Save DataFrame to CSV
+table.to_csv(csv_file_path, index=False)
+
+print(f"CSV file saved successfully at: {csv_file_path}")
